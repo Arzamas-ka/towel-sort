9b3e0f01eea755f586403b33d17174c355b59183
Liudmila Arzamasova <liudmila.arzamasova@gmail.com>
2020-02-22 21:10:04 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix)) {
+    return [];
+  }
+
+  return matrix
+    .map((elem, id) => {
+      if (id === 0) {
+        return elem;
+      }
+      if (id % 2 !== 0) {
+        return elem.reverse();
+      }
+      return elem;
+    })
+    .flat();
+};
